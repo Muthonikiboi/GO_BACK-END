6bc4a06588a83a9e43c7e0c770a176373e4e8ab0
Muthonikiboi <kiboijoye254@gmail.com>
2023-12-12 22:30:37 +0300

    Conversions

--- Simple_Prac/02UserInput/main.go
+++ Simple_Prac/02UserInput/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -11,13 +13,21 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Enter rating for our pizza")
 
-	//store reader into a variable 
-	//no try catch in go 
-	//comma ok // err ok
+	// store reader into a variable
+	// no try-catch in Go
+	// comma ok // err ok
 
-	input,_:=reader.ReadString('\n')
-	fmt.Println("thanks for the ratings ->",input)
-	fmt.Printf(`type of rating is %T`,input)
+	input, _ := reader.ReadString('\n')
+	fmt.Println("thanks for the ratings ->", input)
+	fmt.Printf("type of rating is %T\n", input)
 
-	 
+	// conversions assuming we want to add one to each of the ratings
+
+	numRatings, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Add 1 to your ratings", numRatings+1)
+	}
 }
