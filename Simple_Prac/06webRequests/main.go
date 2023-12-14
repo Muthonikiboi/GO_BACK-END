c99c3321efe7b6020963d0286692bec52d71c1cf
Muthonikiboi <kiboijoye254@gmail.com>
2023-12-14 14:39:45 +0300

    handling web requests

---
+++ Simple_Prac/06webRequests/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+//make url global
+const url = "https://lco.dev"
+
+func main(){
+
+	//make simple request from net package
+	response,err := http.Get(url)
+
+	if err != nil{
+		panic (err)
+	}
+
+	//check type of response
+	fmt.Printf("The type of rensoinse is : %T \n",response)
+
+	//close the public body of the response ie CONNECTION
+	defer response.Body.Close()
+
+	//read the response
+	databyte, err := ioutil.ReadAll(response.Body)
+	
+	if err != nil{
+		panic(err)
+	}
+
+	//check the response in string format
+	content := string(databyte)
+	fmt.Println(content)
+}
